56ca7d66f0b10eb66d32b1e4a2a9da11771e4ea7
Muhammad Mujtaba <muhammad.mujtaba@centre.edu>
2019-09-19 15:36:23 -0400

    work

--- scrape.js
+++ scrape.js
@@ -2,4 +2,18 @@ var scraper = require('./tools').scraper;
 //scraper = function(url,parseFcn,filename)
 //scrapes a URL, parses it with parseFcn, and archives the data in a JSON file called filename.
 
+var getChars = function($,data)
+{
+var parent = $(" #comics-characterindex > section.module.moduColor_Light.modu_AZ > div:nth-child(4) > div > div:nth-child(2)");
+var children = parent.find("li");
+children.toArray()       
+   .forEach(function(item,index)
+    {
+       data.push($(item).text());
+    });
+}
 
+
+var top= "https://www.marvel.com/comics/characters";
+
+scraper(top, getChars, "marvel.json")
